523929f1b2f90213796300fd3949b47f6d522acc
Max Daunarovich <max.daunarovich@gmail.com>
2025-10-14 02:31:38 +0300

    Fix build error

--- src/app/[network]/account/[id]/keys/page.tsx
+++ src/app/[network]/account/[id]/keys/page.tsx
@@ -3,18 +3,12 @@ import AccountKeysContent from "@/app/[network]/account/[id]/keys/content";
 import { Suspense } from "react";
 import JumpingDots from "@/components/flowscan/JumpingDots";
 
-interface AccountKeysProps {
-  params: Promise<{ id: string }>;
-}
 /*--------------------------------------------------------------------------------------------------------------------*/
-export default async function AccountKeys(props: AccountKeysProps) {
-  const params = await props.params;
-  const { id } = params;
-
+export default function AccountKeys() {
   return (
     <div className={"w-full"}>
       <Suspense fallback={<JumpingDots />}>
-        <AccountKeysContent address={id} />
+        <AccountKeysContent />
       </Suspense>
     </div>
   );
