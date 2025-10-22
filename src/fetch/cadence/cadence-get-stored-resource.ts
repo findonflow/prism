60bc6d34274bf398512d59e6c60777c0f5a19ecd
Max Daunarovich <max.daunarovich@gmail.com>
2025-10-23 00:43:14 +0300

    Add new cadence script to get stored resource

---
+++ src/fetch/cadence/cadence-get-stored-resource.ts
@@ -0,0 +1,8 @@
+export const cadenceGetStoredResource = `
+  access(all) fun main(address: Address, pathStr: String): &AnyResource? {
+    let account = getAuthAccount<auth(Storage, Contracts, Keys, Inbox, Capabilities) &Account>(address)
+    let path = StoragePath(identifier: pathStr)!
+    
+    return account.storage.borrow<&AnyResource>(from: path)
+  }
+`
