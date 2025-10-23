0a1a7006a56e557c739bdd35f01577b8b498bce8
Max Daunarovich <max.daunarovich@gmail.com>
2025-10-23 11:54:27 +0300

    Adjust navigation

--- src/app/[network]/account/[id]/navigation.tsx
+++ src/app/[network]/account/[id]/navigation.tsx
@@ -41,20 +41,22 @@ export default function AccountNavigation() {
       <NavigationLink to={""} baseUrl={baseUrl}>
         Linked Accounts
       </NavigationLink>
-      <NavigationLink to={"keys"} baseUrl={baseUrl}>
-        Public Keys
-      </NavigationLink>
+
       <NavigationLink to={"public-storage"} baseUrl={baseUrl}>
         Public Storage
       </NavigationLink>
       <NavigationLink to={"stored-items"} baseUrl={baseUrl}>
         Stored Items
       </NavigationLink>
-      <NavigationLink to={"contracts"} baseUrl={baseUrl}>
-        Contracts
-      </NavigationLink>
       <NavigationLink to={"collections"} baseUrl={baseUrl}>
         Collections
+      </NavigationLink>
+      <span className={"w-[1px] flex-1 self-stretch mx-2 h-auto bg-gray-300"}></span>
+      <NavigationLink to={"keys"} baseUrl={baseUrl}>
+        Public Keys
+      </NavigationLink>
+      <NavigationLink to={"contracts"} baseUrl={baseUrl}>
+        Contracts
       </NavigationLink>
     </div>
   );
