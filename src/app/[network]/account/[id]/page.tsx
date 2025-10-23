ba85238c21ffb78e6d92b0ef682413e1de2e1715
Max Daunarovich <max.daunarovich@gmail.com>
2025-10-23 18:25:01 +0300

    Fix content page for account

--- src/app/[network]/account/[id]/page.tsx
+++ src/app/[network]/account/[id]/page.tsx
@@ -1,13 +1,9 @@
 /*--------------------------------------------------------------------------------------------------------------------*/
-import { Construction } from "lucide-react";
+import LinkedAccountsContent from "@/app/[network]/account/[id]/content";
 
 /*--------------------------------------------------------------------------------------------------------------------*/
 export default async function AccountDetailsPage() {
   return (
-    <div className={"flex flex-row gap-2 items-center text-orange-400 font-light"}>
-      <Construction className={"w-4 h-4"}/>
-      <p>Here we will show information about linked accounts</p>
-      <Construction className={"w-4 h-4"}/>
-    </div>
+    <LinkedAccountsContent />
   );
 }
