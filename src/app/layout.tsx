01ba6a77f3f34951e067855ad36c67f7582da58c
Aritra Roy <aritraroy14.ar@gmail.com>
2025-10-30 14:52:30 +0530

    Login fix when trying to switch network

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import type { Metadata } from "next";
 import "./globals.css";
-import { FCLProvider } from "@/fetch/provider";
 
 /*--------------------------------------------------------------------------------------------------------------------*/
 const geistSans = Geist({
@@ -31,7 +30,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <FCLProvider>{children}</FCLProvider>
+        {children}
       </body>
     </html>
   );
