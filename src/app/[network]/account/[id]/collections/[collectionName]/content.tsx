cbae8a00d16c51dcfc5ffb7f4bb915ba6c887258
Max Daunarovich <max.daunarovich@gmail.com>
2025-10-31 00:00:13 +0300

    Move collection to a different path

--- src/app/[network]/account/[id]/collections/[collectionName]/content.tsx
+++ src/app/[network]/account/[id]/collections/[collectionName]/content.tsx
@@ -1,20 +1,21 @@
 "use client";
+/*--------------------------------------------------------------------------------------------------------------------*/
+import Link from "next/link";
 import SimpleClientPagination from "@/components/flowscan/SimpleClientPagination";
 import { CollectionDetailsHeader, NftCard } from "@/components/ui/collection";
 import useAccountResolver from "@/hooks/useAccountResolver";
 import { useCollectionDetails } from "@/hooks/useCollectionDetails";
 import { useCollectionPathItems } from "@/hooks/useCollectionPathItems";
 import useQueryParams from "@/hooks/utils/useQueryParams";
-import Image from "next/image";
 import { useParams } from "next/navigation";
-import { extractCollectionName } from "../content";
 import { LoadingBlock } from "@/components/flowscan/JumpingDots";
+/*--------------------------------------------------------------------------------------------------------------------*/
 
 export default function CollectionPathContent() {
-  const { network, id, path } = useParams() as Record<string, string>;
+  const { network, id, collectionName } = useParams() as Record<string, string>;
 
   const { data: resolved, isPending: isResolving } = useAccountResolver(
-    id as string
+    id as string,
   );
   const address = resolved?.owner;
 
@@ -22,23 +23,24 @@ export default function CollectionPathContent() {
   const [offset = "0", limit = "25"] = getQueryParams(["offset", "limit"]);
 
   const { data: collectionDetails, isPending: fetchingDetails } =
-    useCollectionDetails(address, path);
+    useCollectionDetails(address, collectionName);
 
   const { data: collectionItems, isPending: fetchingItems } =
-    useCollectionPathItems(address, path);
+    useCollectionPathItems(address, collectionName);
 
   const itemsList = collectionItems?.slice(
     parseInt(offset),
-    parseInt(offset) + parseInt(limit)
+    parseInt(offset) + parseInt(limit),
   );
 
-  if (!address || !path)
+  if (!address || !collectionName)
     return <div>Please provide a valid account identifier.</div>;
+
   if (!collectionDetails && !fetchingDetails)
     return <div>No details found for this collection.</div>;
 
   return (
-    <div className="flex flex-col w-full">
+    <div className="flex w-full flex-col">
       {fetchingDetails ? (
         <LoadingBlock title="Loading Details" />
       ) : (
@@ -51,12 +53,15 @@ export default function CollectionPathContent() {
           totalItems={collectionItems?.length}
         />
       )}
-      <div className="grid mt-4 grid-cols-2 gap-3 md:grid-cols-3 lg:grid-cols-5 xl:grid-cols-6 w-full">
+      <div className="mt-4 grid w-full grid-cols-2 gap-3 md:grid-cols-3 lg:grid-cols-5 xl:grid-cols-6">
         {fetchingItems && <LoadingBlock title="Loading Items" />}
         {itemsList?.map((token: any, i: number) => (
-          <a href={`${path}/${token.tokenId}`} className="contents">
-            <NftCard token={token} key={`${i}-${path}`} address={address} />
-          </a>
+          <Link
+            href={`/${network}/account/${id}/collections/${collectionName}/${token.tokenId}`}
+            className="truncate underline"
+          >
+            <NftCard token={token} key={token.tokenId} />
+          </Link>
         ))}
       </div>
     </div>
