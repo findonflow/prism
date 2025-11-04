0412e119a4802a77c5c43b8c8f89940070186af5
Max Daunarovich <max.daunarovich@gmail.com>
2025-11-04 11:24:42 +0300

    Update header

--- src/components/ui/header/index.tsx
+++ src/components/ui/header/index.tsx
@@ -1,6 +1,7 @@
 import { Pyramid } from "lucide-react";
 import ConnectWallet from "@/components/ui/connect-wallet";
 import Link from "next/link";
+import { cn } from "@/lib/utils";
 
 /*--------------------------------------------------------------------------------------------------------------------*/
 
@@ -8,8 +9,8 @@ function PrismLogo(props: { link: string }) {
   const { link } = props;
   return (
     <Link href={link}>
-      <div className={"flex flex-row text-xl items-center gap-1"}>
-        <Pyramid className={"w-5 h-5"} />
+      <div className={"flex flex-row items-center gap-1 text-xl"}>
+        <Pyramid className={"h-5 w-5"} />
         <span className={"font-bold"}>Prism</span>
       </div>
     </Link>
@@ -20,11 +21,19 @@ function PrismLogo(props: { link: string }) {
 export default function Header(props: { network: string }) {
   const { network } = props;
   return (
-    <div className={"flex flex-row justify-between items-center w-full gap-2"}>
-      <div className={"flex flex-row gap-2 items-center justify-start"}>
+    <div
+      className={cn(
+        "bg-prism-level-1 flex w-full flex-row items-center justify-between gap-2",
+        "border-prism-border border-b",
+        "px-4 py-2"
+      )}
+    >
+      <div className={"flex flex-row items-center justify-start gap-2"}>
         <PrismLogo link={"/"} />
         <span
-          className={"px-2 py-1 text-sm border-prism-border border-1 rounded-sm"}
+          className={
+            "border-prism-primary text-prism-primary rounded-sm border-1 px-2 py-1 text-sm"
+          }
         >
           {network}
         </span>
