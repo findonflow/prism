fa3643ea4ad79f34d188b36cd640b18f3b1cdc1d
Max Daunarovich <max.daunarovich@gmail.com>
2025-11-04 11:35:21 +0300

    Refactor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,5 @@
 /*--------------------------------------------------------------------------------------------------------------------*/
-import {
-  TypeH1,
-  TypeH2,
-  TypeP,
-  TypeTextBlock,
-} from "@/components/ui/typography";
+import { TypeH1, TypeP, TypeTextBlock } from "@/components/ui/typography";
 import Footer from "@/components/ui/footer";
 import Link from "next/link";
 import { cn } from "@/lib/utils";
@@ -14,63 +9,19 @@ import SimpleTag from "@/components/flowscan/SimpleTag";
 import type { ReactNode } from "react";
 
 /*--------------------------------------------------------------------------------------------------------------------*/
-function NetworkSelector(props: {
-  link: string;
-  title: string;
-  copy: string;
-  className?: string;
-  icon: ReactNode;
-}) {
-  const { link, title, copy } = props;
-  const { className, icon } = props;
-
-  return (
-    <Link href={link}>
-      <div
-        className={cn(
-          "flex h-full w-full flex-col items-center justify-center border-1 pt-6 pb-4",
-          "hover:bg-prism-level-2 hover:border-current hover:shadow-[0_0_3rem_0]",
-          "border-current/75 shadow-current/50 shadow-[0_0_0_0]",
-          "md:border-prism-text-muted/40 md:p-6 md:px-8",
-          "group rounded-sm transition-colors duration-500",
-          className,
-        )}
-      >
-        <div className={"flex flex-col items-center justify-center space-y-2"}>
-          <SimpleTag
-            label={title}
-            category={icon}
-            hideArrow
-            className={"text-md px-2"}
-          />
-          <TypeP
-            className={cn(
-              "max-w-[10em] group-hover:text-current",
-              "[.group:not(:hover)_&]:text-prism-text-muted",
-              "transition duration-300",
-            )}
-          >
-            {copy}
-          </TypeP>
-        </div>
-      </div>
-    </Link>
-  );
-}
-
-/*--------------------------------------------------------------------------------------------------------------------*/
 export default function Home() {
   return (
     <div className="flex w-full flex-col items-center text-center">
-      {/* First section - Full height with network selector */}
 
+      {/* Anchor */}
+      <div className={"absolute top-0 "} id={"welcome-to-prism"}></div>
+
+
+      {/* First section - Full height with network selector */}
       <div className={"max-w-5xl px-4 lg:px-6"}>
         <div className="flex h-screen w-full flex-col items-center justify-between">
           <main className="flex h-full flex-col items-center justify-center">
             <TypeTextBlock className={"mb-8"}>
-              {/* Anchor */}
-              <div className={"fixed top-0"} id={"welcome-to-prism"}></div>
-
               <TypeH1
                 className={cn(
                   "inline-flex w-full flex-row flex-wrap items-center justify-center gap-2 font-light",
@@ -131,5 +82,50 @@ export default function Home() {
       {/* Bottom Footer */}
       <Footer />
     </div>
+  );
+}
+
+/*--------------------------------------------------------------------------------------------------------------------*/
+function NetworkSelector(props: {
+  link: string;
+  title: string;
+  copy: string;
+  className?: string;
+  icon: ReactNode;
+}) {
+  const { link, title, copy } = props;
+  const { className, icon } = props;
+
+  return (
+    <Link href={link}>
+      <div
+        className={cn(
+          "flex h-full w-full flex-col items-center justify-center border-1 pt-6 pb-4",
+          "hover:bg-prism-level-2 hover:border-current hover:shadow-[0_0_3rem_0]",
+          "border-current/75 shadow-current/50 shadow-[0_0_0_0]",
+          "md:border-prism-text-muted/40 md:p-6 md:px-8",
+          "group rounded-sm transition-colors duration-500",
+          className,
+        )}
+      >
+        <div className={"flex flex-col items-center justify-center space-y-2"}>
+          <SimpleTag
+            label={title}
+            category={icon}
+            hideArrow
+            className={"text-md px-2"}
+          />
+          <TypeP
+            className={cn(
+              "max-w-[10em] group-hover:text-current",
+              "[.group:not(:hover)_&]:text-prism-text-muted",
+              "transition duration-300",
+            )}
+          >
+            {copy}
+          </TypeP>
+        </div>
+      </div>
+    </Link>
   );
 }
