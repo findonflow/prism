9b341165f801dc93123918229c17260cc61a5434
Max Daunarovich <max.daunarovich@gmail.com>
2025-11-06 00:07:10 +0300

    Add more tag variants

--- src/components/ui/tags/index.tsx
+++ src/components/ui/tags/index.tsx
@@ -1,7 +1,23 @@
 /*--------------------------------------------------------------------------------------------------------------------*/
-import {cn} from "@/lib/utils";
-import {Package} from "lucide-react";
-import {formatNumberToAccounting} from "@/lib/format";
+import {
+  Cpu,
+  FileX2,
+  Flame,
+  HandCoins,
+  Hash,
+  KeyRound,
+  OctagonAlert,
+  Package,
+  PackageCheck,
+  ShieldUser,
+  Signature,
+  VenetianMask,
+} from "lucide-react";
+import { formatNumberToAccounting } from "@/lib/format";
+import SimpleTag from "@/components/flowscan/SimpleTag";
+import { cn } from "@/lib/utils";
+import { getErrorInfo } from "@/consts/error-codes";
+import { NODE_TITLES } from "@/consts/node";
 
 /*--------------------------------------------------------------------------------------------------------------------*/
 export function NumberOfItems(props: { items?: number }) {
@@ -35,7 +51,221 @@ export function VaultBalance(props: { balance?: string; symbol?: string }) {
       )}
     >
       <span className={"opacity-75"}>{symbol}</span>
-      <b className={cn("text-md")}>{formatNumberToAccounting(formatted, 4, 2)}</b>
+      <b className={cn("text-md")}>
+        {formatNumberToAccounting(formatted, 4, 2)}
+      </b>
     </div>
+  );
+}
+
+/*--------------------------------------------------------------------------------------------------------------------*/
+export function TagGas(props: { gas?: number; title?: string }) {
+  const { gas, title } = props;
+  const formattedGas = formatNumberToAccounting(gas);
+  const finalTitle = title || `Gas: ${formattedGas}`;
+
+  if (!gas) {
+    return null;
+  }
+
+  return (
+    <SimpleTag
+      title={finalTitle}
+      label={`Gas: ${formattedGas}`}
+      className={"text-bsn-label hover:text-orange-300"}
+      category={<Flame />}
+    />
+  );
+}
+
+/*--------------------------------------------------------------------------------------------------------------------*/
+export function TagMultisig(props: {
+  signers?: Array<string>;
+  short?: boolean;
+}) {
+  const { signers, short } = props;
+
+  if (!signers) {
+    return null;
+  }
+
+  const numSigners = signers.length;
+  const title = `This transaction was signed by ${numSigners} signers`;
+
+  const label = short ? numSigners : `Multisig: ${numSigners}`;
+  const hoverLabel = short
+    ? numSigners.toString()
+    : `Multisig: ${signers.join(", ")}`;
+
+  return (
+    <SimpleTag
+      title={title}
+      label={label}
+      hoverLabel={hoverLabel}
+      className={"text-flsn-unusual"}
+      category={<Signature />}
+    />
+  );
+}
+
+/* --------------------------------------------------------------------------------------------- */
+export function TagStaker(props: { balance?: number }) {
+  const { balance } = props;
+
+  if (!balance) {
+    return null;
+  }
+
+  const staked = formatNumberToAccounting(balance);
+
+  return (
+    <SimpleTag
+      label={<ShieldUser />}
+      className={"text-orange-300"}
+      title={`Staker: This account has staked ${staked} FLOW to secure network stability`}
+    />
+  );
+}
+
+/* --------------------------------------------------------------------------------------------- */
+export function TagDelegator(props: { balance?: number }) {
+  const { balance } = props;
+
+  if (!balance) {
+    return null;
+  }
+
+  const delegated = formatNumberToAccounting(balance);
+
+  return (
+    <SimpleTag
+      label={<HandCoins />}
+      className={"text-orange-300"}
+      title={`Delegator: This account has delegated ${delegated} FLOW to other nodes`}
+    />
+  );
+}
+
+/*--------------------------------------------------------------------------------------------------------------------*/
+export function TagExecEffort(props: { effort?: number; title?: string }) {
+  const { effort } = props;
+
+  if (!effort) {
+    return null;
+  }
+
+  const precision = parseInt(effort.toString().split("-")[1]);
+  const formatted = effort.toFixed(precision);
+  const title = `Execution Effort: ${formatted}`;
+
+  if (!effort) {
+    return null;
+  }
+
+  return (
+    <SimpleTag
+      title={title}
+      label={`${formatted}`}
+      className={"text-bsn-label"}
+      category={<Cpu />}
+    />
+  );
+}
+
+/* --------------------------------------------------------------------------------------------- */
+export function TagNonce(props: { nonce?: number; title?: string }) {
+  const { nonce, title } = props;
+  const tagTitle = title || `Proposer Sequence Number: ${nonce}`;
+
+  if (!nonce) {
+    return null;
+  }
+
+  return (
+    <SimpleTag
+      title={tagTitle}
+      label={nonce}
+      className={"text-bsn-label"}
+      category={<Hash />}
+    />
+  );
+}
+
+/* --------------------------------------------------------------------------------------------- */
+export function TagKey(props: { keyIndex?: number; title?: string }) {
+  const { keyIndex, title } = props;
+  const tagTitle = title || `Proposer Key Index: ${keyIndex}`;
+
+  if (!keyIndex) {
+    return null;
+  }
+
+  return (
+    <SimpleTag
+      title={tagTitle}
+      label={keyIndex}
+      className={"text-bsn-label"}
+      category={<KeyRound />}
+    />
+  );
+}
+
+/*--------------------------------------------------------------------------------------------------------------------*/
+export function TagFlowStatus(props: {
+  status?: string;
+  error?: string;
+  errorCode?: string;
+}) {
+  const { status } = props;
+  const { error, errorCode } = props;
+
+  const { title, description } = getErrorInfo(errorCode);
+
+  return !error ? (
+    <SimpleTag
+      label={status || "SEALED"}
+      category={<PackageCheck />}
+      className={"text-bsn-success"}
+    />
+  ) : (
+    <div className={"cursor-pointer"}>
+      <SimpleTag
+        label={title}
+        title={description}
+        category={<OctagonAlert />}
+        className={"text-bsn-failure"}
+      />
+    </div>
+  );
+}
+
+/* --------------------------------------------------------------------------------------------- */
+export function TagNodeType(props: { role?: string }) {
+  const { role } = props;
+
+  if (!role) {
+    return null;
+  }
+
+  const title = NODE_TITLES[role as keyof typeof NODE_TITLES] || "";
+
+  return (
+    <SimpleTag
+      label={role}
+      title={`Role: ${title}`}
+      className={"text-bsn-label capitalize"}
+      category={<VenetianMask />}
+    />
+  );
+}
+
+/* --------------------------------------------------------------------------------------------- */
+export function TagNotFound() {
+  return (
+    <SimpleTag
+      label={"Not found"}
+      className={"text-bsn-failure"}
+      category={<FileX2 />}
+    />
   );
 }
---
+++ src/consts/error-codes.ts
@@ -0,0 +1,79 @@
+const ERROR_CODES = {
+  "1006": {
+    title: "INVALID_KEY",
+    description: "One of the keys used in this transaction is not valid.",
+  },
+  "1007": {
+    title: "NOT_SUBMITTED",
+    description:
+      "This transaction was not submitted because it was sent to the blockchain with a key that already has a pending transaction.",
+  },
+  "1008": {
+    title: "INVALID_KEY",
+    description: "One of the keys used in this transaction is not valid.",
+  },
+  "1009": {
+    title: "INVALID_KEY",
+    description:
+      "This transaction was not submitted because it was sent to the blockchain with a key that is invalid. Double check the signing/hashing algorithms.",
+  },
+  "1052": {
+    title: "WRONG_ARGUMENTS",
+    description: "One of the arguments sent into this transaction is wrong.",
+  },
+  "1055": {
+    title: "NOT_AUTHORISED",
+    description: "This transaction does not have enough signatures to be sent.",
+  },
+  "1056": {
+    title: "NOT_PERMITTED",
+    description: "This transaction is trying to do something it is not allowed to do.",
+  },
+  	"1101": {
+		title: "CODE_ERROR",
+		description: "There is an error in the cadence code of this transaction.",
+	},
+  "1103": {
+    title: "NO_STORAGE",
+    description:
+      "One of the accounts in this transaction does not have enough flow to pay for storage. Ensure that the account you are adding data to has enough flow before trying again.",
+  },
+  "1105": {
+    title: "TOO_MANY_EVENTS",
+    description: "This transaction is too complex, it tries to send to many events.",
+  },
+  "1110": {
+    title: "TOO_COMPLEX",
+    description:
+      "This transaction is too complex it does too much logic or read/write to much data.",
+  },
+  "1111": {
+    title: "TOO_COMPLEX",
+    description: "This transaction ran out of memory in the execution phase.",
+  },
+  "1118": {
+    title: "CANNOT_PAY",
+    description: "The payer of this transaction does not have enough funds to send it.",
+  },
+  "1201": {
+    title: "ACCOUNT_NOT_FOUND",
+    description: "One account referred to in this transaction cannot be found.",
+  },
+  "1202": {
+    title: "INVALID_KEY",
+    description: "One of the keys used in this transaction is not valid.",
+  },
+};
+
+const GENERIC_ERROR = {
+  title: "Error",
+  description: "Whoops! 😨 Something unexpected happened...",
+};
+
+export function getErrorInfo(code: string | undefined) {
+  if (!code) {
+    return GENERIC_ERROR;
+  }
+
+  return ERROR_CODES[code as keyof typeof ERROR_CODES] || GENERIC_ERROR;
+}
