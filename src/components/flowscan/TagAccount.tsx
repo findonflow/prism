79a75fa5cbc0702a0767781d3dcccbbf6c9f6437
Max Daunarovich <max.daunarovich@gmail.com>
2025-11-06 00:08:16 +0300

    Fix TagAccount

--- src/components/flowscan/TagAccount.tsx
+++ src/components/flowscan/TagAccount.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 import { useParams } from "next/navigation";
 import { UserRound } from "lucide-react";
 import SimpleTag from "@/components/flowscan/SimpleTag";
+import { withPrefix } from "@/lib/validate";
 
 /*--------------------------------------------------------------------------------------------------------------------*/
 interface TagAccountProps {
@@ -15,15 +16,18 @@ interface TagAccountProps {
 export default function TagAccount(props: TagAccountProps) {
   const { address, className, title } = props;
   const { network } = useParams();
-  const href = `/${network}/account/${address}`;
+
+  const fixedAddress = withPrefix(address);
+
+  const href = `/${network}/account/${fixedAddress}`;
 
   return (
     <Link href={href}>
       <SimpleTag
-        label={address}
+        label={fixedAddress}
         category={<UserRound className="h-4 w-4" />}
         className={className}
-        title={title || `View account ${address}`}
+        title={title || `View account ${fixedAddress}`}
       />
     </Link>
   );
