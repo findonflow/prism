705ed6fb3e48966b6024515d7e5c5cb02ac92e86
Max Daunarovich <max.daunarovich@gmail.com>
2025-12-08 02:52:46 +0300

    Add Cadence code for owned account display

---
+++ src/mutate/cadence/cadence-mutate-set-owned-display.ts
@@ -0,0 +1,20 @@
+export const cadenceMutateSetOwnedDisplay = `
+import HybridCustody from 0xHybridCustody
+import MetadataViews from 0xMetadataViews
+
+transaction(name: String, description: String, thumbnail: String) {
+  prepare(acct: auth(Storage) &Account) {
+    let o = acct.storage.borrow<auth(HybridCustody.Owner) &HybridCustody.OwnedAccount>(from: HybridCustody.OwnedAccountStoragePath)
+  ?? panic("account not found")
+
+    let d = MetadataViews.Display(
+      name: name,
+      description: description,
+      thumbnail: MetadataViews.HTTPFile(url: thumbnail)
+  )
+
+    o.setDisplay(d)
+  }
+}
+ 
+`
